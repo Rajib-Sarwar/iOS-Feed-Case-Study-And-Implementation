0f22abda7c99e9ebe62e863ae077cdbb8636571a
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2022-08-01 13:52:29 -0400

    Added FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Chowdhury Md Rajib  Sarwar on 1/8/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let ImageURL: String
+}
