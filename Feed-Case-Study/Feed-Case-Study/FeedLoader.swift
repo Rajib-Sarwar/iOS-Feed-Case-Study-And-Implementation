aa5d3ec514e780a14444442801f6d81c0b4f423b
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2022-08-01 12:39:28 -0400

    Create the FeedLoader protocol boundary

---
+++ Feed-Case-Study/Feed-Case-Study/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  Feed-Case-Study
+//
+//  Created by Chowdhury Md Rajib  Sarwar on 1/8/22.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping(LoadFeedResult) -> Void)
+}
