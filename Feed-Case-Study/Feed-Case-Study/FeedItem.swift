4179a68107e906539078e0d645ba794b029558b7
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2022-08-01 12:32:49 -0400

    Create the FeedItem model

---
+++ Feed-Case-Study/Feed-Case-Study/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Feed-Case-Study
+//
+//  Created by Chowdhury Md Rajib  Sarwar on 1/8/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
